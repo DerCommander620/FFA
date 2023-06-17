966c0db6691ce8a9877d15d677dfdc73bfed9a6c
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-17 21:42:10 +0200

    Update Main.php

--- src/FFA/Main.php
+++ src/FFA/Main.php
@@ -1,21 +1,22 @@
 <?php
+
 namespace FFA;
 
 use pocketmine\plugin\PluginBase;
-use FFA\Commands\FFACommand;
 use pocketmine\utils\SingletonTrait;
+use FFA\Commands\FFACommand;
 
 class Main extends PluginBase{
     use SingletonTrait;
 
     public function onLoad(): void{
         self::setInstance($this);
-        self::saveConfig();
+        $this->saveDefaultConfig();
     }
 
     public function onEnable(): void{
-        self::getServer()->getCommandMap()->registerAll($this->getName(), $this[
+        $this->getServer()->getCommandMap()->registerAll($this->getName(), [
             new FFACommand()
         ]);
     }
-} 
+}
